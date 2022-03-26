0414caa90ab5637d0f030545837b0e93019a0838
tigger <fronttigger@gmail.com>
2022-03-27 00:28:36 +0900

    feat: zzzz

--- scripts/canary-publish.sh
+++ scripts/canary-publish.sh
@@ -2,7 +2,7 @@
 pkg_version=`node -e "console.log(require('./package.json').version.split('-')[0])"`
 git_commit_sha=`git rev-parse --short HEAD`
 
-# npx npm@7 version $pkg_version-$git_commit_sha --workspaces
+
 npm run version --workspaces $pkg_version-$git_commit_sha
 # npx npm@7 run build:all
 npm run build:all
